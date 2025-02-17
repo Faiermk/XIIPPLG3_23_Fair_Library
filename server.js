b741966ee8dd27647fe66eede2f6ba5ea60c2214
Faiermk <pubgreceh15@gmail.com>
2025-02-17 11:47:51 +0700

    create category

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/database');
+const categoriesRoutes = require('./routes/categories')
 const authRoutes = require('./routes/auth');  // Tambahkan ini
 
 dotenv.config();
@@ -11,6 +12,7 @@ connectDB();
 
 // Gunakan route auth
 app.use('/api/auth', authRoutes);
+app.use('/api/kategori', categoriesRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
